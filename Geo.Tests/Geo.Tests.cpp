06d9a10d21c75dfb34f92ba5bc96ce37d0f1ff03
sinner4real <kovorv_tema@mail.ru>
2024-11-27 14:48:49 +0300

    version 1

---
+++ Geo.Tests/Geo.Tests.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../triangle/figure.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GeoTests
+{
+	TEST_CLASS(GeoTests)
+	{
+	public:
+		/*
+		TEST_METHOD(Negative_Side_Expected_Exeption)
+		{
+			Assert::ExpectException<std::out_of_range>([]() {auto triangle = new figure(-1, 1, 2); });
+		}
+
+		TEST_METHOD(Get_Perimetr_Correct)
+		{
+			figure triangle{3, 4, 5};
+			const double expected = 12;
+			const double epsilon = 0.1;
+
+			const double actual = triangle.GetPerimetr();
+
+			Assert::IsTrue(abs(expected - actual) <= epsilon);
+
+		}*/
+	};
+}
